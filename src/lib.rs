058d6fa08629ef5c26de2516e37c2ea837025e21
Alexis Bourget <alexis.bourget@gmail.com>
2020-10-29 03:26:34 +0100

    Use BitStorage in GrowableBitMap and test it with u8, u16, u32, u64 and u128

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-//! A crate providing a growable compact boolean array.
+//! A crate providing a growable compact boolean array that can be
+//! parameterized on its storage.
 //!
 //! See the `GrowableBitMap` type for more information.
 //!
@@ -14,8 +15,6 @@
 //! - `BitXor` (with another `GrowableBitMap`).
 //! - `BitXorAssign` (with another `GrowableBitMap`).
 //!
-//! - All fixed unsigned integers as storage (`u16`, `u32`, `u64` and `u128`
-//!   are missing).
 //! - When `const-generics` become available, possibly use them as storage ?
 //!
 //! - [Rust 1.48.0+ / Intra-doc links]: Use intra-doc links in documentation.
@@ -25,12 +24,13 @@ use std::fmt;
 
 mod storage;
 pub use storage::BitStorage;
-use storage::BITS_IN_BYTE;
 
-/// A growable compact boolean array.
+/// A growable compact boolean array that can be parameterized on its storage.
 ///
 /// Bits are stored contiguously. The first value is packed into the least
 /// significant bits of the first word of the backing storage.
+///
+/// The storage must implement the unsafe trait `BitStorage`.
 ///
 /// # Caveats
 ///
@@ -39,23 +39,28 @@ use storage::BITS_IN_BYTE;
 ///   the bits between 1200 and 1400). In this case, storing the offset of
 ///   1200 somewhere else and storing the values in the range `0..=200` in the
 ///   `GrowableBitMap` is probably the most efficient solution.
-/// - Right now the only implemented storage integer is `u8`.
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub struct GrowableBitMap {
+pub struct GrowableBitMap<S>
+where
+    S: BitStorage,
+{
     // The storage for the bits.
-    bits: Vec<u8>,
+    bits: Vec<S>,
 }
 
-impl fmt::Debug for GrowableBitMap {
+impl<S> fmt::Debug for GrowableBitMap<S>
+where
+    S: BitStorage + fmt::Debug,
+{
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         fmt.debug_list().entries(self.bits.iter()).finish()
     }
 }
 
-impl GrowableBitMap {
-    // Number of bits that can be stored in one instance of the backend type.
-    const BITS_BY_STORAGE: usize = 8;
-
+impl<S> GrowableBitMap<S>
+where
+    S: BitStorage,
+{
     /// Creates a new, empty `GrowableBitMap`.
     ///
     /// This does not allocate anything.
@@ -63,11 +68,11 @@ impl GrowableBitMap {
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// assert!(GrowableBitMap::new().is_empty());
+    /// assert!(GrowableBitMap::<u8>::new().is_empty());
     /// ```
-    pub const fn new() -> Self {
+    pub fn new() -> Self {
         Self { bits: Vec::new() }
     }
 
@@ -85,54 +90,54 @@ impl GrowableBitMap {
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// let mut b = GrowableBitMap::with_capacity(8);
+    /// let mut b = GrowableBitMap::<u16>::with_capacity(16);
     /// assert!(b.is_empty());
-    /// assert_eq!(b.capacity(), 8);
+    /// assert_eq!(b.capacity(), 16);
     ///
-    /// b.set_bit(7);
-    /// assert_eq!(b.capacity(), 8);
+    /// b.set_bit(15);
+    /// assert_eq!(b.capacity(), 16);
     ///
-    /// b.set_bit(10);
-    /// assert!(b.capacity() >= 8);
+    /// b.set_bit(17);
+    /// assert!(b.capacity() >= 16);
     /// ```
     pub fn with_capacity(capacity: usize) -> Self {
         if capacity == 0 {
             return Self::new();
         }
 
-        let div = capacity / Self::BITS_BY_STORAGE;
+        let div = capacity / S::BITS_IN_STORAGE;
         // Ensures the allocated capacity is enough for values like 125 with a
         // storage of `u8`:
         //
         // - `div` is 15
-        // - `capacity % Self::BITS_BY_STORAGE` is 5 so `rem` is 1.
+        // - `capacity % S::BITS_IN_STORAGE` is 5 so `rem` is 1.
         //
         // The final capacity will be 16 `u8`s -> 128 bits, enough for the
         // 125 bits asked for.
-        let rem = (capacity % Self::BITS_BY_STORAGE != 0) as usize;
+        let rem = (capacity % S::BITS_IN_STORAGE != 0) as usize;
 
         Self {
             bits: Vec::with_capacity(div + rem),
         }
     }
 
-    /// `true` if the GrowableBitMap is empty.
+    /// `true` if the `GrowableBitMap` is empty.
     ///
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// let mut b = GrowableBitMap::new();
+    /// let mut b = GrowableBitMap::<u32>::new();
     /// assert!(b.is_empty());
     ///
-    /// b.set_bit(3);
+    /// b.set_bit(25);
     /// assert!(!b.is_empty());
     /// ```
     pub fn is_empty(&self) -> bool {
-        self.bits.is_empty() || self.bits.iter().all(|bits| *bits == 0)
+        self.bits.is_empty() || self.bits.iter().all(|bits| bits.is_empty())
     }
 
     /// Gets the bit at the given index and returns `true` when it is set to 1,
@@ -144,9 +149,9 @@ impl GrowableBitMap {
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// let mut b = GrowableBitMap::new();
+    /// let mut b = GrowableBitMap::<u64>::new();
     /// assert!(!b.get_bit(0));
     /// assert!(!b.get_bit(15));
     ///
@@ -155,7 +160,7 @@ impl GrowableBitMap {
     /// assert!(b.get_bit(15));
     /// ```
     pub fn get_bit(&self, index: usize) -> bool {
-        let bits_index = index / Self::BITS_BY_STORAGE;
+        let bits_index = index / S::BITS_IN_STORAGE;
 
         // Since the bits_index does not exist in the storage, the bit at
         // `index` is logically 0.
@@ -163,10 +168,19 @@ impl GrowableBitMap {
             return false;
         }
 
-        let elem = self.bits[bits_index];
-        let mask = 1 << (index - bits_index * BITS_IN_BYTE);
+        let elem = &self.bits[bits_index];
 
-        (elem & mask) != 0
+        // SAFETY: we have ensure throught the steps above that the index
+        // passed to `elem.set_bit` is in range of `0..S::BITS_IN_STORAGE`.
+        //
+        // Example with a `u8`:
+        //
+        // `u8::BITS_IN_STORAGE` is 8.
+        // `index` is 21.
+        //
+        // `bits_index` = 2
+        // `index - bits_index * S::BITS_IN_STORAGE` = 21 - 2 * 8 = 5 < 8
+        unsafe { elem.get_bit(index - bits_index * S::BITS_IN_STORAGE) }
     }
 
     /// Sets the bit at the given index and returns whether the bit was set
@@ -182,31 +196,35 @@ impl GrowableBitMap {
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// let mut b = GrowableBitMap::new();
+    /// let mut b = GrowableBitMap::<u128>::new();
     /// assert!(b.set_bit(0)); // Bit 0 was not set before, returns true.
     /// assert!(!b.set_bit(0)); // Bit 0 was already set, returns false.
     ///
-    /// assert!(b.set_bit(10)); // The bitmap will grow as needed to set the bit.
+    /// assert!(b.set_bit(255)); // The bitmap will grow as needed to set the bit.
     /// ```
     pub fn set_bit(&mut self, index: usize) -> bool {
-        let bits_index = index / Self::BITS_BY_STORAGE;
+        let bits_index = index / S::BITS_IN_STORAGE;
 
         // Ensure there are enough elements in the `bits` storage.
         if self.bits.len() <= bits_index {
-            self.bits.resize(bits_index + 1, 0);
+            self.bits.resize_with(bits_index + 1, S::empty);
         }
 
         let elem = &mut self.bits[bits_index];
 
-        let mask = 1 << (index - bits_index * BITS_IN_BYTE);
-        let prev = *elem & mask;
-
-        *elem |= mask;
-
-        // If prev is 0, it means the bit was set by this call.
-        prev == 0
+        // SAFETY: we have ensure throught the steps above that the index
+        // passed to `elem.set_bit` is in range of `0..S::BITS_IN_STORAGE`.
+        //
+        // Example with a `u8`:
+        //
+        // `u8::BITS_IN_STORAGE` is 8.
+        // `index` is 21.
+        //
+        // `bits_index` = 2
+        // `index - bits_index * S::BITS_IN_STORAGE` = 21 - 2 * 8 = 5 < 8
+        unsafe { elem.set_bit(index - bits_index * S::BITS_IN_STORAGE) }
     }
 
     /// Clears the bit at the given index and returns whether the bit was set
@@ -219,16 +237,29 @@ impl GrowableBitMap {
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// let mut b = GrowableBitMap::new();
-    /// assert!(!b.clear_bit(0)); // Bit 0 was not set before, returns false.
+    /// let mut b = GrowableBitMap::<u8>::new();
+    /// assert!(!b.clear_bit(3)); // Bit 0 was not set before, returns false.
     ///
-    /// b.set_bit(0);
-    /// assert!(b.clear_bit(0));
+    /// b.set_bit(3);
+    /// assert!(b.clear_bit(3));
+    /// ```
+    ///
+    /// Testing the effects on capacity:
+    ///
+    /// ```rust
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
+    ///
+    /// let mut b = GrowableBitMap::<u8>::new();
+    /// b.set_bit(125);
+    ///
+    /// let old_capa = b.capacity();
+    /// b.clear_bit(125);
+    /// assert_eq!(old_capa, b.capacity());
     /// ```
     pub fn clear_bit(&mut self, index: usize) -> bool {
-        let bits_index = index / Self::BITS_BY_STORAGE;
+        let bits_index = index / S::BITS_IN_STORAGE;
 
         // Since the bits_index does not exist in the storage, the bit at
         // `index` is logically 0.
@@ -238,29 +269,47 @@ impl GrowableBitMap {
 
         let elem = &mut self.bits[bits_index];
 
-        let mask = 1 << (index - bits_index * BITS_IN_BYTE);
-        let prev = *elem | !mask;
-
-        *elem &= !mask;
-
-        prev != 0
+        // SAFETY: we have ensure throught the steps above that the index
+        // passed to `elem.set_bit` is in range of `0..S::BITS_IN_STORAGE`.
+        //
+        // Example with a `u8`:
+        //
+        // `u8::BITS_IN_STORAGE` is 8.
+        // `index` is 21.
+        //
+        // `bits_index` = 2
+        // `index - bits_index * S::BITS_IN_STORAGE` = 21 - 2 * 8 = 5 < 8
+        unsafe { elem.clear_bit(index - bits_index * S::BITS_IN_STORAGE) }
     }
 
-    /// Clears the bitmap, removing all values.
+    /// Clears the bitmap, removing all values (setting them all to 0).
     ///
     /// This method has no effect on the allocated capacity of the bitmap.
     ///
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// let mut b = GrowableBitMap::new();
+    /// let mut b = GrowableBitMap::<u16>::new();
     /// b.set_bit(4);
     /// assert!(!b.is_empty());
     ///
     /// b.clear();
     /// assert!(b.is_empty());
+    /// ```
+    ///
+    /// Testing the effects on capacity:
+    ///
+    /// ```rust
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
+    ///
+    /// let mut b = GrowableBitMap::<u16>::new();
+    /// b.set_bit(125);
+    ///
+    /// let old_capa = b.capacity();
+    /// b.clear();
+    /// assert_eq!(old_capa, b.capacity());
     /// ```
     pub fn clear(&mut self) {
         self.bits.clear();
@@ -271,9 +320,9 @@ impl GrowableBitMap {
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// let mut b = GrowableBitMap::new();
+    /// let mut b = GrowableBitMap::<u32>::new();
     /// assert_eq!(b.count_ones(), 0);
     ///
     /// b.set_bit(2);
@@ -281,11 +330,14 @@ impl GrowableBitMap {
     ///
     /// b.set_bit(9);
     /// assert_eq!(b.count_ones(), 2);
+    ///
+    /// b.clear();
+    /// assert_eq!(b.count_ones(), 0);
     /// ```
     pub fn count_ones(&self) -> usize {
         self.bits
             .iter()
-            .map(|&store| store.count_ones() as usize)
+            .map(|store| store.count_ones() as usize)
             .sum::<usize>()
     }
 
@@ -294,16 +346,16 @@ impl GrowableBitMap {
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// let mut b = GrowableBitMap::new();
+    /// let mut b = GrowableBitMap::<u64>::new();
     /// assert_eq!(b.capacity(), 0);
     ///
-    /// b.set_bit(125);
-    /// assert_eq!(b.capacity(), 128);
+    /// b.set_bit(380);
+    /// assert_eq!(b.capacity(), 384);
     /// ```
     pub fn capacity(&self) -> usize {
-        self.bits.capacity() * Self::BITS_BY_STORAGE
+        self.bits.capacity() * S::BITS_IN_STORAGE
     }
 
     /// Shrinks the capacity of the `GrowableBitMap` as much as possible.
@@ -315,17 +367,17 @@ impl GrowableBitMap {
     /// # Examples
     ///
     /// ```rust
-    /// use growable_bitmap::GrowableBitMap;
+    /// use growable_bitmap::{GrowableBitMap, BitStorage};
     ///
-    /// let mut b = GrowableBitMap::with_capacity(125);
+    /// let mut b = GrowableBitMap::<u128>::with_capacity(381);
     ///
-    /// b.set_bit(63);
     /// b.set_bit(127);
-    /// assert_eq!(b.capacity(), 128);
+    /// b.set_bit(380);
+    /// assert_eq!(b.capacity(), 384);
     ///
-    /// b.clear_bit(127);
+    /// b.clear_bit(380);
     /// b.shrink_to_fit();
-    /// assert_eq!(b.capacity(), 64);
+    /// assert_eq!(b.capacity(), 128);
     /// ```
     pub fn shrink_to_fit(&mut self) {
         // Ignoring the values at the end that are 0.
@@ -333,9 +385,196 @@ impl GrowableBitMap {
             .bits
             .iter()
             .rev()
-            .skip_while(|&&store| store == 0)
+            .skip_while(|&store| store.is_empty())
             .count();
+
         self.bits.truncate(last_set_bit_index);
         self.bits.shrink_to_fit();
     }
+}
+
+macro_rules! growable_bitmap_storage_integer_tests {
+    ($(($int: ty, $mod_name: ident))*) => {
+        $(
+            #[cfg(test)]
+            mod $mod_name {
+                use super::*;
+
+                #[test]
+                fn new() {
+                    let bm = GrowableBitMap::<$int>::new();
+                    let bm2 = GrowableBitMap {
+                        bits: Vec::<$int>::new(),
+                    };
+
+                    assert_eq!(bm, bm2);
+                }
+
+                #[test]
+                fn with_capacity() {
+                    let bm = GrowableBitMap::<$int>::with_capacity(0);
+                    let vec = Vec::<$int>::with_capacity(0);
+                    assert_eq!(bm.bits.capacity(), vec.capacity());
+
+                    let bm = GrowableBitMap::<$int>::with_capacity(11);
+                    let vec = Vec::<$int>::with_capacity(11);
+                    assert!(bm.bits.capacity() >= vec.capacity() / <$int>::BITS_IN_STORAGE);
+                }
+
+                #[test]
+                fn is_empty() {
+                    let bm = GrowableBitMap::<$int>::new();
+                    assert!(bm.is_empty());
+
+                    let mut bm = GrowableBitMap::<$int>::with_capacity(3);
+                    assert!(bm.is_empty());
+
+                    bm.set_bit(7);
+                    assert!(!bm.is_empty());
+
+                    bm.clear_bit(7);
+                    assert!(bm.is_empty());
+
+                    bm.set_bit(7);
+                    bm.set_bit(3);
+                    bm.set_bit(4);
+                    assert!(!bm.is_empty());
+
+                    bm.clear();
+                    assert!(bm.is_empty());
+                }
+
+                #[test]
+                fn get_bit() {
+                    let bm = GrowableBitMap::<$int>::new();
+
+                    assert!(!bm.get_bit(0));
+                    assert!(!bm.get_bit(7));
+
+                    // Ensuring `get_bit` does not allocate.
+                    let old_capa = bm.capacity();
+                    assert!(!bm.get_bit(200000003));
+                    assert_eq!(old_capa, bm.capacity());
+
+                    let mut bm = bm;
+                    bm.set_bit(0);
+                    bm.set_bit(6);
+
+                    assert!(bm.get_bit(0));
+                    assert!(bm.get_bit(6));
+
+                    // Still false.
+                    assert!(!bm.get_bit(7));
+                    assert!(!bm.get_bit(3));
+                }
+
+                #[test]
+                fn set_bit() {
+                    let mut bm = GrowableBitMap::<$int>::new();
+
+                    assert!(bm.set_bit(0));
+                    assert!(bm.set_bit(7));
+
+                    assert!(!bm.set_bit(0));
+
+                    // Ensuring `set_bit` does allocate when necessary.
+                    let old_capa = bm.capacity();
+                    assert!(bm.set_bit(150));
+                    assert!(old_capa <= bm.capacity());
+                    assert!(bm.get_bit(150));
+                }
+
+                #[test]
+                fn clear_bit() {
+                    let mut bm = GrowableBitMap::<$int>::new();
+
+                    assert!(!bm.clear_bit(0));
+                    assert!(!bm.clear_bit(7));
+
+                    bm.set_bit(0);
+                    assert!(bm.clear_bit(0));
+
+                    // Ensuring `clear_bit` does not allocate nor free.
+                    let old_capa = bm.capacity();
+                    assert!(!bm.clear_bit(200000003));
+                    assert_eq!(old_capa, bm.capacity());
+
+                    bm.set_bit(150);
+                    assert!(bm.clear_bit(150));
+
+                    assert!(bm.is_empty());
+                }
+
+                #[test]
+                fn clear() {
+                    let mut bm = GrowableBitMap::<$int>::new();
+
+                    bm.clear();
+                    assert!(bm.is_empty());
+
+                    bm.set_bit(253);
+                    // Ensuring `clear_bit` does not allocate nor free.
+                    let old_capa = bm.capacity();
+                    bm.clear();
+                    assert_eq!(old_capa, bm.capacity());
+
+                    bm.set_bit(30);
+                    bm.set_bit(4);
+
+                    bm.clear();
+                    assert!(bm.is_empty());
+               }
+
+                #[test]
+                fn count_ones() {
+                    let mut bm = GrowableBitMap::<$int>::new();
+                    assert_eq!(bm.count_ones(), 0);
+
+                    bm.set_bit(0);
+                    assert_eq!(bm.count_ones(), 1);
+
+                    bm.set_bit(30);
+                    assert_eq!(bm.count_ones(), 2);
+
+                    bm.set_bit(7);
+                    assert_eq!(bm.count_ones(), 3);
+
+                    bm.clear_bit(0);
+                    assert_eq!(bm.count_ones(), 2);
+
+                    bm.clear();
+                    assert_eq!(bm.count_ones(), 0);
+                }
+
+                #[test]
+                fn capacity() {
+                    let mut bm = GrowableBitMap::<$int>::new();
+                    assert_eq!(bm.capacity(), 0);
+
+                    bm.set_bit(511);
+                    assert_eq!(bm.capacity(), 512);
+                }
+
+                #[test]
+                fn shrink_to_fit() {
+                    let mut bm = GrowableBitMap::<$int>::with_capacity(381);
+                    bm.set_bit(127);
+                    bm.set_bit(380);
+                    assert_eq!(bm.capacity(), 384);
+
+                    bm.clear_bit(380);
+                    bm.shrink_to_fit();
+                    assert_eq!(bm.capacity(), 128);
+                }
+            }
+         )*
+    }
+}
+
+growable_bitmap_storage_integer_tests! {
+    (u8, test_u8)
+    (u16, test_u16)
+    (u32, test_u32)
+    (u64, test_u64)
+    (u128, test_u128)
 }
